0df153e13218e90cb26010124ea7279ab8b4be0a
Aaron Tolentino <aarontolentinotrejo@gmail>
2025-11-16 10:55:07 -0800

    deploy proof of ownership

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  meta: {
+    name: 'impact-site-verification',
+    value: 'e694bdac-49fd-47bc-856e-945eea174664',
+  },
+};
 
 export default nextConfig;
